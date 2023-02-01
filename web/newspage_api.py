b978db89adbec8a39b31439d8c143360b732257a
Jibin-Song <sjbpsh1@sookmyung.ac.kr>
2023-02-01 21:28:42 +0900

    revise UI

--- web/newspage_api.py
+++ web/newspage_api.py
@@ -1,5 +1,3 @@
-from db_connect import db
-
 from flask import (Blueprint, Flask, abort, g, jsonify, make_response,
                    redirect, render_template, request, session, url_for)
 
--- web/show_news_in_portal.py
+++ web/show_news_in_portal.py
@@ -1,3 +1,3 @@
-# DB 사용 안 하면 csv로 가져온다.
+# DB 사용 안 한다면 csv로 가져오기
 import pandas as pd
 df = pd.read_csv('crawling\data\portal_news.csv')
